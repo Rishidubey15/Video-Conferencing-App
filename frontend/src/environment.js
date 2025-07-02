2fe8f01b1afc243e840eafca3c70ad2abc61a079
Rishidubey15 <dubey.rishi66.rd@gmail.com>
2025-07-02 11:19:00 +0530

    Fixed server url

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,5 +1,4 @@
 let IS_PROD = true;
 
-const server = IS_PROD ? "https://video-conferencing-app-uze9.onrender.com" : "http://localhost:8000";
+export const server = IS_PROD ? "https://video-conferencing-app-uze9.onrender.com" : "http://localhost:8000";
 
-export default { server };
