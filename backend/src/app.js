fd510b07398bdd7946ff6135bbd2dce1bd68ca05
Rishidubey15 <dubey.rishi66.rd@gmail.com>
2025-07-02 10:50:00 +0530

    Fixed await-mongoose spacing

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,12 +19,15 @@ app.use(express.urlencoded({limit: "40kb", extended: true}))
 app.use("/api/v1/users", userRoutes);
 
 const start = async () => {
-  const connectionDb = awaitmongoose.connect(process.env.MONGODB_URI, {
+  const connectionDb = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     ssl: true, // Ensure SSL is on
     tlsAllowInvalidCertificates: false,
-  });;
+  });
+
+  mongoose.set("debug", true);
+
   console.log(`Connected to mongodb: ${connectionDb}`);
   server.listen(app.get("port"), () => {
     console.log(`Server is running on port ${app.get("port")}`);
